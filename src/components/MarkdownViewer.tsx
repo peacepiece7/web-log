c3257dcc933d29eb74caa9677e4af52310ef90ba
peacepiece7 <peacepiece7@naver.com>
2023-06-28 17:48:40 +0900

    fix : uppercase typo 수정

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import './markdownViewer.css'
+import './MarkdownViewer.css'
 
 export default function MarkdownViewer({ html }: { html: string }) {
   return (
